5739f9cd53742f4ae86736316176f1a83c425161
Richard LE PENNEC <richard.lepennec@l-acoustics.com>
2020-05-14 14:19:03 +0200

    Roll

--- deadlands/script/deadlands.js
+++ deadlands/script/deadlands.js
@@ -1,7 +1,7 @@
 /**
- * The Dice class is the class used to roll dices.
+ * The Roll class is the class used to dice roll.
  */
-class Dice {
+class Roll {
 
 	/**
 	 * Constructor.
@@ -9,6 +9,8 @@ class Dice {
 	constructor() {}
 
 	/**
+	 * Rolls the specified type of dice.
+	 * @param dice The type of dice to roll.
 	 * @return the specified closed roll.
 	 */
 	static closed(dice) {
@@ -16,11 +18,48 @@ class Dice {
 	}
 
 	/**
+	 * Rolls the specified type of dice.
+	 * @param dice The type of dice to roll.
 	 * @return the specified open roll.
 	 */
 	static open(dice) {
 		var roll = randomInteger(dice);
 		return roll === dice ? roll + this.open(dice) : roll;
+	}
+
+}
+
+/**
+ * The Rolls class is the class to manage pool of rolls.
+ */
+class Rolls {
+
+	/**
+	 * Constructor.
+	 */
+	constructor() {}
+
+	/**
+	 * Gets the maximum roll.
+	 * @param rolls The rolls to watch.
+	 * @return the maximum roll.
+	 */
+	static max(rolls) {
+		return _.max(rolls);
+	}
+
+	/**
+	 * Rolls the specified dices.
+	 * @param size The number of dice to roll.
+	 * @param dice The type of dice to roll.
+	 * @return the dices rolls.
+	 */
+	static open(size, dice) {
+		var rolls = [];
+		for (var i=0; i<size; i+=1) {
+			rolls.push(Roll.open(dice));
+		}
+		return rolls;
 	}
 
 }
@@ -62,8 +101,19 @@ class Deadlands {
 		if (cmd != 'dl') return;
 		log("Handle Deadlands command");
 
-		log(Dice.open(6));
+		log(Rolls.open(4, 6));
 
+	}
+
+	/**
+	 * Handles the specified roll.
+	 * @param size      The number of dices to roll.
+	 * @param dice     The type of dices to roll.
+	 * @param modifier The modifier to apply on the roll.
+	 * @param tn       The target number.
+	 */
+	openRoll(size, dice, modifier, tn) {
+		
 	}
 
 }
