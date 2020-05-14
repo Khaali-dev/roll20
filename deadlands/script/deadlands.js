b5c706cb06375aa8d5f1b88dc651266a0f4f4053
Richard LE PENNEC <richard.lepennec@l-acoustics.com>
2020-05-14 21:09:07 +0200

    Improve dice management

--- deadlands/script/deadlands.js
+++ deadlands/script/deadlands.js
@@ -1,65 +1,171 @@
 /**
- * The Roll class is the class used to dice roll.
+ * The Tool class is an utility class with static methods.
  */
-class Roll {
+class Tools {
 
 	/**
-	 * Constructor.
+	 * Creates a string with ordered items. Each item will be separated by the specified separator.
+	 * @param items     The items to serialize.
+	 * @param separator The item separator.
+	 * @return the string.
 	 */
-	constructor() {}
-
-	/**
-	 * Rolls the specified type of dice.
-	 * @param dice The type of dice to roll.
-	 * @return the specified closed roll.
-	 */
-	static closed(dice) {
-		return randomInteger(dice);
-	}
-
-	/**
-	 * Rolls the specified type of dice.
-	 * @param dice The type of dice to roll.
-	 * @return the specified open roll.
-	 */
-	static open(dice) {
-		var roll = randomInteger(dice);
-		return roll === dice ? roll + this.open(dice) : roll;
+	static toString(items, separator) {
+		var string = "";
+		if (items.length > 0) {
+			string += items[0];
+			for (var i=1; i<items.length; i++) {
+				string += separator;
+				string += items[i];
+			}
+		}
+		return string;
 	}
 
 }
 
 /**
- * The Rolls class is the class to manage pool of rolls.
+ * The Dice class is the class used to manage dice roll.
  */
+class Dice {
+
+	/**
+	 * Constructor.
+	 * @param side The number of side.
+	 */
+	constructor(side) {
+		this.side = side;
+		this.open = true;
+	}
+
+	/**
+	 * Open the dice.
+	 * @return the dice.
+	 */
+	open() {
+		this.open = true;
+		return this;
+	}
+
+	/**
+	 * Closes the dice.
+	 * @return the dice.
+	 */
+	close() {
+		this.open = false;
+		return this;
+	}
+
+	/**
+	 * @return the dice roll.
+	 */
+	roll() {
+		var value = randomInteger(this.side);
+		return this.open === true && this.side === value ? value + this.roll() : value;
+	}
+
+	/**
+	 * @return the textual expression of the instance.
+	 */
+	toString() {
+		return "d" + this.side;
+	}
+
+}
+
+/**
+ * The Dices class is the class to manage pool of dices. Dices can be rolled to get rolls.
+ */
+class Dices {
+
+	/**
+	 * Constructor.
+	 */
+	constructor() {
+		this.dices = new Map();
+	}
+
+	/**
+	 * Removes all dices.
+	 * @eturn the instance.
+	 */
+	clear() {
+		this.dices = new Map();
+		return this;
+	}
+
+	/**
+	 * Adds the specified dices.
+	 * @param dice The type of dice to add.
+	 * @param size The number of dices to add.
+	 * @eturn the instance.
+	 */
+	add(dice, size) {
+		this.dices.set(dice, size);
+		return this;
+	}
+
+	/**
+	 * @return a dices rolls.
+	 */
+	roll() {
+		var rolls = new Rolls();
+		for (var [dice,size] of this.dices) {
+			for (var i=0; i<size; i++) {
+				rolls.add(new Roll(dice, dice.roll()));
+			}
+		}
+		return rolls;
+	}
+
+}
+
+class Roll {
+
+	/**
+	 * Constructor.
+	 * @param dice  The rolled dice.
+	 * @param value The roll value.
+	 */
+	constructor(dice, value) {
+		this.dice = dice;
+		this.value = value;
+	}
+
+	/**
+	 * @return the textual expression of the instance.
+	 */
+	toString() {
+		return this.value + "(" + this.dice.toString() + ")";
+	}
+
+}
+
 class Rolls {
 
 	/**
 	 * Constructor.
 	 */
-	constructor() {}
+	constructor() {
+		this.rolls = [];
 
-	/**
-	 * Gets the maximum roll.
-	 * @param rolls The rolls to watch.
-	 * @return the maximum roll.
-	 */
-	static max(rolls) {
-		return _.max(rolls);
 	}
 
 	/**
-	 * Rolls the specified dices.
-	 * @param size The number of dice to roll.
-	 * @param dice The type of dice to roll.
-	 * @return the dices rolls.
+	 * Adds the specified roll.
+	 * @param roll The roll to add.
+	 * @eturn the instance.
 	 */
-	static open(size, dice) {
-		var rolls = [];
-		for (var i=0; i<size; i+=1) {
-			rolls.push(Roll.open(dice));
-		}
-		return rolls;
+	add(roll) {
+		this.rolls.push(roll);
+		return this;
+	}
+
+
+	/**
+	 * @return the textual expression of the rolls.
+	 */
+	toString() {
+		return Tools.toString(this.rolls, ", ");
 	}
 
 }
@@ -87,8 +193,7 @@ class Deadlands {
 		if (cmd != 'dl') return;
 		log("Handle Deadlands command");
 
-		
-		this.openRoll(4, 6, 0, 5);
+		this.openRoll(2, 0, 0, 0);
 
 	}
 
@@ -101,13 +206,19 @@ class Deadlands {
 	 */
 	openRoll(size, dice, modifier, tn) {
 		log("openRoll");
+
+		var rolls = new Dices()
+			.add(new Dice(8), 2)
+			.add(new Dice(6), 4)
+			.roll();
+	
 		var html = "";
 		html += "<div class='sheet-rolltemplate-default'>";
 		html += "<table>";
-		html += "<caption>Jet " + size + " d " + dice + " with TN " + tn + "</caption>";
+		html += "<caption>Jet " + size + "d" + dice + " avec SD " + tn + "</caption>";
 		html += "<tr>";
-		html += "<td>Resultat</td>";
-		html += "<td>" + Rolls.open(size, dice) + "</td>";
+		html += "<td>Dés:</td>";
+		html += "<td>" + rolls.toString() + "</td>";
 		html += "</tr>";
 		html += "</table>";
 		html += "</div>";
