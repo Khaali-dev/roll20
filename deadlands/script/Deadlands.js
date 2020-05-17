9b87cc2ffc4b5b03559d35ac2b140d538f6ef722
Richard LE PENNEC <richard.lepennec@l-acoustics.com>
2020-05-17 13:45:35 +0200

    Cleanup

--- deadlands/script/Deadlands.js
+++ deadlands/script/Deadlands.js
@@ -1,31 +1,66 @@
-class DeadlandsRolls extends Rolls {
+class DeadlandsRolls {
 
 	/**
-	 * @constructor
+	 * Indicates if the specified rolls is a fumble.
+	 * @parma rolls The rolls to interpret.
+	 * @return true if the rolls is a fumble.
 	 */
-	constructor() {
-		super();
+	static isFumble(rolls) {
+		return rolls.size() === 0 ? false : parseFloat(rolls.numberOf(1)) >= parseFloat(rolls.size()/2);
 	}
 
 	/**
 	 * Gets the number of success.
-	 * -1 : Fumble
-	 *  0 : Fail
-	 *  1 : Success
-	 *  2 : Success with one degree
-	 *  N : Success with N-1 degrees
-	 * @param tn  The difficulty target number.
-	 * @param mod The roll modifier.
-	 * @return the number of success.
+	 * @param value The rolls value to interpret.
+	 * @param tn    The difficulty target number.
+	 * @return the number rof success:
+	 *     0 : Fail
+	 *     1 : Success
+	 *     2 : Success with one degree
+	 *     N : Success with N-1 degrees
 	 */
-	result(tn, mod) {
-		if (super.size() === 0 ? false : parseFloat(super.numberOf(1)) >= parseFloat(this.rolls.length/2)) {
-			return -1;
+	static getSuccess(value, tn) {
+		return value < tn ? 0 : Math.floor(value/5);
+	}
+
+	/**
+	 * Gets the number of success for the specified skill roll.
+	 * @param rolls    The rolls to interpret.
+	 * @param tn       The difficulty target number.
+	 * @param modifier The modifier to apply to the roll.
+	 * @return the number rof success:
+	 *    -1 : Fumble
+	 *     0 : Fail
+	 *     1 : Success
+	 *     2 : Success with one degree
+	 *     N : Success with N-1 degrees
+	 */
+	static skill(rolls, tn, modifier) {
+		return DeadlandsRolls.isFumble(rolls) ? -1 : DeadlandsRolls.getSuccess(rolls.max() + modifier, tn);
+	}
+
+	/**
+	 * Gets the number of success for the specified unskill roll.
+	 * @param rolls    The rolls to interpret.
+	 * @param tn       The difficulty target number.
+	 * @param modifier The modifier to apply to the roll.
+	 * @return the number rof success:
+	 *    -1 : Fumble
+	 *     0 : Fail
+	 *     1 : Success
+	 *     2 : Success with one degree
+	 *     N : Success with N-1 degrees
+	 */
+	static unskill(rolls, tn, modifier) {
+		return DeadlandsRolls.isFumble(rolls) ? -1 : DeadlandsRolls.getSuccess(Math.floor(rolls.max()/2) + modifier, tn);
+	}
+
+	static rollSkill(attribute, coordination, skill, tn, modifier) {
+		if (skill > 0) {
+			return skill(new Dices(attribute, skill).rolls(), tn, modifier);
+		} else {
+			return unskill(new Dices(attribute, coordination).rolls(), tn, modifier);
 		}
-		if (!super.atLeast(tn-mod)) {
-			return 0;
-		}
-		return Math.floor((this.max()-tn+mod)/5) + 1;
 	}
 
 }
@@ -58,12 +93,14 @@ class Deadlands extends AbstractMessageHandler {
 		this.openRoll(2, 0, 0, 0);
 	}
 
+
+
 	/**
 	 * Handles the specified roll.
-	 * @param size     The number of dices to roll.
-	 * @param dice     The type of dices to roll.
-	 * @param modifier The modifier to apply on the roll.
-	 * @param tn       The target number.
+	 * @param attribute    The attribute defines the number of dices to roll.
+	 * @param coordination The skill or the coordination defines the type of dices to roll.
+	 * @param modifier     The modifier to apply to the roll.
+	 * @param tn           The difficulty target number.
 	 */
 	openRoll(size, dice, modifier, tn) {
 		log("openRoll");
@@ -72,7 +109,7 @@ class Deadlands extends AbstractMessageHandler {
 			.add(new Dice(8), 2)
 			.add(new Dice(6), 4)
 			.roll();
-	
+
 		var html = "";
 		html += "<div class='sheet-rolltemplate-default'>";
 		html += "<table>";
--- deadlands/script/TestSuite.js
+++ deadlands/script/TestSuite.js
@@ -1,183 +1,243 @@
 var _deadlands = _deadlands || new TestSuite("deadlands")
 	.add("Rolls as fumble without modifier", () => {
-		return new DeadlandsRolls()
+		const rolls = new Rolls()
 			.add(new Roll(new Dice(4), 1))
 			.add(new Roll(new Dice(4), 1))
 			.add(new Roll(new Dice(6), 1))
 			.add(new Roll(new Dice(6), 6))
-			.add(new Roll(new Dice(6), 2))
-			.result(5, 0) === -1;
+			.add(new Roll(new Dice(6), 2));
+		const fumble = DeadlandsRolls.isFumble(rolls);
+		const skill = DeadlandsRolls.skill(rolls, 5, 0) === -1;
+		const unskill = DeadlandsRolls.unskill(rolls, 5, 0) === -1;
+		return fumble && skill && unskill;
 	})
 	.add("Rolls as fumble with modifier", () => {
-		return new DeadlandsRolls()
+		const rolls = new Rolls()
 			.add(new Roll(new Dice(4), 1))
 			.add(new Roll(new Dice(4), 1))
 			.add(new Roll(new Dice(6), 1))
 			.add(new Roll(new Dice(6), 6))
-			.add(new Roll(new Dice(6), 2))
-			.result(5, 10) === -1;
+			.add(new Roll(new Dice(6), 2));
+		const fumble = DeadlandsRolls.isFumble(rolls);
+		const skill = DeadlandsRolls.skill(rolls, 5, 10) === -1;
+		const unskill = DeadlandsRolls.unskill(rolls, 5, 10) === -1;
+		return fumble && skill && unskill;
 	})
 	.add("Rolls as fail without modifier", () => {
-		return new DeadlandsRolls()
+		const rolls = new Rolls()
 			.add(new Roll(new Dice(4), 1))
 			.add(new Roll(new Dice(4), 3))
 			.add(new Roll(new Dice(6), 1))
 			.add(new Roll(new Dice(6), 4))
-			.add(new Roll(new Dice(6), 2))
-			.result(5, 0) === 0;
+			.add(new Roll(new Dice(6), 2));
+		const fumble = DeadlandsRolls.isFumble(rolls);
+		const skill = DeadlandsRolls.skill(rolls, 5, 0) === 0;
+		const unskill = DeadlandsRolls.unskill(rolls, 5, 0) === 0;
+		return !fumble && skill && unskill;
 	})
 	.add("Rolls as fail with bonus", () => {
-		return new DeadlandsRolls()
+		const rolls = new Rolls()
 			.add(new Roll(new Dice(4), 1))
 			.add(new Roll(new Dice(4), 3))
 			.add(new Roll(new Dice(6), 1))
 			.add(new Roll(new Dice(6), 3))
-			.add(new Roll(new Dice(6), 2))
-			.result(5, 1) === 0;
+			.add(new Roll(new Dice(6), 2));
+		const fumble = DeadlandsRolls.isFumble(rolls);
+		const skill = DeadlandsRolls.skill(rolls, 5, 1) === 0;
+		const unskill = DeadlandsRolls.unskill(rolls, 5, 1) === 0;
+		return !fumble && skill && unskill;
 	})
 	.add("Rolls as fail with malus", () => {
-		return new DeadlandsRolls()
+		const rolls = new Rolls()
 			.add(new Roll(new Dice(4), 1))
 			.add(new Roll(new Dice(4), 3))
 			.add(new Roll(new Dice(6), 1))
 			.add(new Roll(new Dice(6), 5))
-			.add(new Roll(new Dice(6), 2))
-			.result(5, -1) === 0;
+			.add(new Roll(new Dice(6), 2));
+		const fumble = DeadlandsRolls.isFumble(rolls);
+		const skill = DeadlandsRolls.skill(rolls, 5, -1) === 0;
+		const unskill = DeadlandsRolls.unskill(rolls, 5, -1) === 0;
+		return !fumble && skill && unskill;
 	})
 	.add("Rolls as success without modifier where max equal to target", () => {
-		return new DeadlandsRolls()
+		const rolls = new Rolls()
 			.add(new Roll(new Dice(4), 1))
 			.add(new Roll(new Dice(4), 3))
 			.add(new Roll(new Dice(6), 1))
 			.add(new Roll(new Dice(6), 5))
-			.add(new Roll(new Dice(6), 2))
-			.result(5, 0) === 1;
+			.add(new Roll(new Dice(6), 2));
+		const fumble = DeadlandsRolls.isFumble(rolls);
+		const skill = DeadlandsRolls.skill(rolls, 5, 0) === 1;
+		const unskill = DeadlandsRolls.unskill(rolls, 5, 0) === 0;
+		return !fumble && skill && unskill;
 	})
 	.add("Rolls as success with bonus where max equal to target", () => {
-		return new DeadlandsRolls()
+		const rolls = new Rolls()
 			.add(new Roll(new Dice(4), 1))
 			.add(new Roll(new Dice(4), 3))
 			.add(new Roll(new Dice(6), 1))
 			.add(new Roll(new Dice(6), 4))
-			.add(new Roll(new Dice(6), 2))
-			.result(5, 1) === 1;
+			.add(new Roll(new Dice(6), 2));
+		const fumble = DeadlandsRolls.isFumble(rolls);
+		const skill = DeadlandsRolls.skill(rolls, 5, 1) === 1;
+		const unskill = DeadlandsRolls.unskill(rolls, 5, 1) === 0;
+		return !fumble && skill && unskill;
 	})
 	.add("Rolls as success with malus where max equal to target", () => {
-		return new DeadlandsRolls()
+		const rolls = new Rolls()
 			.add(new Roll(new Dice(4), 1))
 			.add(new Roll(new Dice(4), 3))
 			.add(new Roll(new Dice(6), 1))
 			.add(new Roll(new Dice(6), 6))
-			.add(new Roll(new Dice(6), 2))
-			.result(5, -1) === 1;
+			.add(new Roll(new Dice(6), 2));
+		const fumble = DeadlandsRolls.isFumble(rolls);
+		const skill = DeadlandsRolls.skill(rolls, 5, -1) === 1;
+		const unskill = DeadlandsRolls.unskill(rolls, 5, -1) === 0;
+		return !fumble && skill && unskill;
 	})
 	.add("Rolls as success without modifier where max equal to one degree minus 1", () => {
-		return new DeadlandsRolls()
+		const rolls = new Rolls()
 			.add(new Roll(new Dice(4), 1))
 			.add(new Roll(new Dice(4), 3))
 			.add(new Roll(new Dice(6), 1))
 			.add(new Roll(new Dice(6), 9))
-			.add(new Roll(new Dice(6), 2))
-			.result(5, 0) === 1;
+			.add(new Roll(new Dice(6), 2));
+		const fumble = DeadlandsRolls.isFumble(rolls);
+		const skill = DeadlandsRolls.skill(rolls, 5, 0) === 1;
+		const unskill = DeadlandsRolls.unskill(rolls, 5, 0) === 0;
+		return !fumble && skill && unskill;
 	})
 	.add("Rolls as success with bonus where max equal to one degree minus 1", () => {
-		return new DeadlandsRolls()
+		const rolls = new Rolls()
 			.add(new Roll(new Dice(4), 1))
 			.add(new Roll(new Dice(4), 3))
 			.add(new Roll(new Dice(6), 1))
 			.add(new Roll(new Dice(6), 8))
-			.add(new Roll(new Dice(6), 2))
-			.result(5, 1) === 1;
+			.add(new Roll(new Dice(6), 2));
+		const fumble = DeadlandsRolls.isFumble(rolls);
+		const skill = DeadlandsRolls.skill(rolls, 5, 1) === 1;
+		const unskill = DeadlandsRolls.unskill(rolls, 5, 1) === 1;
+		return !fumble && skill && unskill;
 	})
 	.add("Rolls as success with malus where max equal to one degree minus 1", () => {
-		return new DeadlandsRolls()
+		const rolls = new Rolls()
 			.add(new Roll(new Dice(4), 1))
 			.add(new Roll(new Dice(4), 3))
 			.add(new Roll(new Dice(6), 1))
 			.add(new Roll(new Dice(6), 10))
-			.add(new Roll(new Dice(6), 2))
-			.result(5, -1) === 1;
+			.add(new Roll(new Dice(6), 2));
+		const fumble = DeadlandsRolls.isFumble(rolls);
+		const skill = DeadlandsRolls.skill(rolls, 5, -1) === 1;
+		const unskill = DeadlandsRolls.unskill(rolls, 5, -1) === 0;
+		return !fumble && skill && unskill;
 	})
 	.add("Rolls as success without modifier where max equal to one degree", () => {
-		return new DeadlandsRolls()
+		const rolls = new Rolls()
 			.add(new Roll(new Dice(4), 1))
 			.add(new Roll(new Dice(4), 3))
 			.add(new Roll(new Dice(6), 1))
 			.add(new Roll(new Dice(6), 10))
-			.add(new Roll(new Dice(6), 2))
-			.result(5, 0) === 2;
+			.add(new Roll(new Dice(6), 2));
+		const fumble = DeadlandsRolls.isFumble(rolls);
+		const skill = DeadlandsRolls.skill(rolls, 5, 0) === 2;
+		const unskill = DeadlandsRolls.unskill(rolls, 5, 0) === 1;
+		return !fumble && skill && unskill;
 	})
-		.add("Rolls as success with bonus where max equal to one degree", () => {
-		return new DeadlandsRolls()
+	.add("Rolls as success with bonus where max equal to one degree", () => {
+		const rolls = new Rolls()
 			.add(new Roll(new Dice(4), 1))
 			.add(new Roll(new Dice(4), 3))
 			.add(new Roll(new Dice(6), 1))
 			.add(new Roll(new Dice(6), 9))
-			.add(new Roll(new Dice(6), 2))
-			.result(5, 1) === 2;
+			.add(new Roll(new Dice(6), 2));
+		const fumble = DeadlandsRolls.isFumble(rolls);
+		const skill = DeadlandsRolls.skill(rolls, 5, 1) === 2;
+		const unskill = DeadlandsRolls.unskill(rolls, 5, 1) === 1;
+		return !fumble && skill && unskill;
 	})
 	.add("Rolls as success with malus where max equal to one degree", () => {
-		return new DeadlandsRolls()
+		const rolls = new Rolls()
 			.add(new Roll(new Dice(4), 1))
 			.add(new Roll(new Dice(4), 3))
 			.add(new Roll(new Dice(6), 1))
 			.add(new Roll(new Dice(6), 11))
-			.add(new Roll(new Dice(6), 2))
-			.result(5, -1) === 2;
+			.add(new Roll(new Dice(6), 2));
+		const fumble = DeadlandsRolls.isFumble(rolls);
+		const skill = DeadlandsRolls.skill(rolls, 5, -1) === 2;
+		const unskill = DeadlandsRolls.unskill(rolls, 5, -1) === 0;
+		return !fumble && skill && unskill;
 	})
 	.add("Rolls as success without modifier where max equal to two degrees minus 1", () => {
-		return new DeadlandsRolls()
+		const rolls = new Rolls()
 			.add(new Roll(new Dice(4), 1))
 			.add(new Roll(new Dice(4), 3))
 			.add(new Roll(new Dice(6), 1))
 			.add(new Roll(new Dice(6), 14))
-			.add(new Roll(new Dice(6), 2))
-			.result(5, 0) === 2;
+			.add(new Roll(new Dice(6), 2));
+		const fumble = DeadlandsRolls.isFumble(rolls);
+		const skill = DeadlandsRolls.skill(rolls, 5, 0) === 2;
+		const unskill = DeadlandsRolls.unskill(rolls, 5, 0) === 1;
+		return !fumble && skill && unskill;
 	})
 	.add("Rolls as success with bonus where max equal to two degrees minus 1", () => {
-		return new DeadlandsRolls()
+		const rolls = new Rolls()
 			.add(new Roll(new Dice(4), 1))
 			.add(new Roll(new Dice(4), 3))
 			.add(new Roll(new Dice(6), 1))
 			.add(new Roll(new Dice(6), 13))
-			.add(new Roll(new Dice(6), 2))
-			.result(5, 1) === 2;
+			.add(new Roll(new Dice(6), 2));
+		const fumble = DeadlandsRolls.isFumble(rolls);
+		const skill = DeadlandsRolls.skill(rolls, 5, 1) === 2;
+		const unskill = DeadlandsRolls.unskill(rolls, 5, 1) === 1;
+		return !fumble && skill && unskill;
 	})
 	.add("Rolls as success with malus where max equal to two degrees minus 1", () => {
-		return new DeadlandsRolls()
+		const rolls = new Rolls()
 			.add(new Roll(new Dice(4), 1))
 			.add(new Roll(new Dice(4), 3))
 			.add(new Roll(new Dice(6), 1))
 			.add(new Roll(new Dice(6), 15))
-			.add(new Roll(new Dice(6), 2))
-			.result(5, -1) === 2;
+			.add(new Roll(new Dice(6), 2));
+		const fumble = DeadlandsRolls.isFumble(rolls);
+		const skill = DeadlandsRolls.skill(rolls, 5, -1) === 2;
+		const unskill = DeadlandsRolls.unskill(rolls, 5, -1) === 1;
+		return !fumble && skill && unskill;
 	})
 	.add("Rolls as success without modifier where max equal to two degrees", () => {
-		return new DeadlandsRolls()
+		const rolls = new Rolls()
 			.add(new Roll(new Dice(4), 1))
 			.add(new Roll(new Dice(4), 3))
 			.add(new Roll(new Dice(6), 1))
 			.add(new Roll(new Dice(6), 15))
-			.add(new Roll(new Dice(6), 2))
-			.result(5, 0) === 3;
+			.add(new Roll(new Dice(6), 2));
+		const fumble = DeadlandsRolls.isFumble(rolls);
+		const skill = DeadlandsRolls.skill(rolls, 5, 0) === 3;
+		const unskill = DeadlandsRolls.unskill(rolls, 5, 0) === 1;
+		return !fumble && skill && unskill;
 	})
 	.add("Rolls as success with bonus where max equal to two degrees", () => {
-		return new DeadlandsRolls()
+		const rolls = new Rolls()
 			.add(new Roll(new Dice(4), 1))
 			.add(new Roll(new Dice(4), 3))
 			.add(new Roll(new Dice(6), 1))
 			.add(new Roll(new Dice(6), 14))
-			.add(new Roll(new Dice(6), 2))
-			.result(5, 1) === 3;
+			.add(new Roll(new Dice(6), 2));
+		const fumble = DeadlandsRolls.isFumble(rolls);
+		const skill = DeadlandsRolls.skill(rolls, 5, 1) === 3;
+		const unskill = DeadlandsRolls.unskill(rolls, 5, 1) === 1;
+		return !fumble && skill && unskill;
 	})
 	.add("Rolls as success with malus where max equal to two degrees", () => {
-		return new DeadlandsRolls()
+		const rolls = new Rolls()
 			.add(new Roll(new Dice(4), 1))
 			.add(new Roll(new Dice(4), 3))
 			.add(new Roll(new Dice(6), 1))
 			.add(new Roll(new Dice(6), 16))
-			.add(new Roll(new Dice(6), 2))
-			.result(5, -1) === 3;
+			.add(new Roll(new Dice(6), 2));
+		const fumble = DeadlandsRolls.isFumble(rolls);
+		const skill = DeadlandsRolls.skill(rolls, 5, -1) === 3;
+		const unskill = DeadlandsRolls.unskill(rolls, 5, -1) === 1;
+		return !fumble && skill && unskill;
 	})
 	;
 
